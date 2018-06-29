c74acda5a0312823a96288baf88d6b2ef60d2012
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-06-29 15:54:22 -0700

    switch to wand

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule WandTest.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: Mix.Tasks.WandCore.Deps.run([])
     ]
   end
 
@@ -15,13 +15,6 @@ defmodule WandTest.MixProject do
   def application do
     [
       extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      {:poison, "~> 3.1"}
     ]
   end
 end
